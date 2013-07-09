c6ed683f8f430d991babb415b0ad9045f3caa87a
szj <bio@Bio-System-Product-Name.(none)>
2013-07-09 19:23:28 +0800

    ^13Jul08_matchup_2Col 49 in matchup.out

--- script.R
+++ script.R
@@ -11,29 +11,33 @@
 {
     ref.AGI.todo <- read.table("ref.AGI.raw.txt", sep = "\t", header = T, na.strings = "")  
     ### set class
-    #ref.AGI.todo[, 2:17] <- as.character(ref.AGI.todo[, 2:17])
+    #ref.AGI.todo[, 2] <- as.character(ref.AGI.todo[, 2])
+    #ref.AGI.todo[, 3] <- as.character(ref.AGI.todo[, 3])
 }
 
 # matchup AGI between 2011MSB & Affy
-matchup <- function(seq.data = seq.AGI, ref.data = ref.AGI.todo, seq.col = 1) {
-    matchup.out <- data.frame()
+matchup <- function(seq.data = seq.AGI, ref.data = ref.AGI.todo, seq.col = 1, ref.col = 2) {
+    matchup.out <- data.frame(stringsAsFactors = FALSE)
+    #names(matchup.out) <- c('Probe.Set.ID', 'AGI')
     for (i in c(1:dim(seq.data)[1])) {
         seq <- seq.data[i, seq.col]
-        loc <- which(ref.data == seq)
+        loc <- which(ref.data == seq); i; seq; loc
         if (length(loc) != 0) {
             col <- loc %/% dim(ref.data)[1]
-            row <- loc %% dim(ref.data)[1]
-            matchup.row <- ref.data[row, ]
-            matchup.out <- rbind(matchup.out, matchup.row)
+            row <- loc %% dim(ref.data)[1]; col; row
+            #matchup.row <- data.frame(as.character(ref.data[row, ref.col]), as.character(seq)); matchup.row
+            matchup.row <- data.frame(ref.data[row, ref.col], seq); matchup.row
+            matchup.out <- rbind(matchup.out, matchup.row); matchup.out
         }
     }
     return(matchup.out)
 }
 ## test matchup
 {
-seq.test <- data.frame(c("ATMG00880", "AT2G07768", "ATMG00960")), c(1,0,1))  ## ref.data[5,3],[6,3][6,4]
+seq.test <- data.frame(c("ATMG00880", "AT2G07768", "ATMG00960"), c(1,0,1))  ## ref.data[5,3],[6,3][6,4]
 seq.test[,1] <- as.character(seq.test[,1])
 matchup(seq.test)
+matchup(seq.AGI[c(1:50), ])
 }
 ## run matchup
-matchup()
+matchup.out <- matchup()

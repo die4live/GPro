680f7865933249ad2aba5574b22827fcd24fbab3
szj <bio@Bio-System-Product-Name.(none)>
2013-07-09 17:36:06 +0800

    ^13Jul08_matchup.between.2011MSB.Affy!done

---
+++ script.R
@@ -0,0 +1,39 @@
+# data import 
+## AGI of interest from 2011MSB
+{
+    seq.AGI <- read.table("seq.AGI.txt", sep = "\t", header = T)
+    ### set class
+    seq.AGI$AGI <- as.character(seq.AGI$AGI)
+    seq.AGI$GPA1.Prey <- as.factor(seq.AGI$GPA1.Prey)
+    seq.AGI$AGB1.Prey <- as.factor(seq.AGI$AGB1.Prey)
+}
+## AGI ref from Affy
+{
+    ref.AGI.todo <- read.table("ref.AGI.raw.txt", sep = "\t", header = T, na.strings = "")  
+    ### set class
+    #ref.AGI.todo[, 2:17] <- as.character(ref.AGI.todo[, 2:17])
+}
+
+# matchup AGI between 2011MSB & Affy
+matchup <- function(seq.data = seq.AGI, ref.data = ref.AGI.todo, seq.col = 1) {
+    matchup.out <- data.frame()
+    for (i in c(1:dim(seq.data)[1])) {
+        seq <- seq.data[i, seq.col]
+        loc <- which(ref.data == seq)
+        if (length(loc) != 0) {
+            col <- loc %/% dim(ref.data)[1]
+            row <- loc %% dim(ref.data)[1]
+            matchup.row <- ref.data[row, ]
+            matchup.out <- rbind(matchup.out, matchup.row)
+        }
+    }
+    return(matchup.out)
+}
+## test matchup
+{
+seq.test <- data.frame(c("ATMG00880", "AT2G07768", "ATMG00960")), c(1,0,1))  ## ref.data[5,3],[6,3][6,4]
+seq.test[,1] <- as.character(seq.test[,1])
+matchup(seq.test)
+}
+## run matchup
+matchup()
